c1764d4b7a6d453eaab4d883bdf1ac3fbff96755
Maciej Wolański <maciekwski@gmail.com>
2017-02-05 22:56:18 +0100

    First part of adding mocked services

---
+++ stage3/application/src/main/java/external/MockStudyDescriptionProviderService.java
@@ -0,0 +1,13 @@
+package external;
+
+/**
+ * Created by Maciej Wolański
+ * maciekwski@gmail.com
+ * on 05.02.2017.
+ */
+public class MockStudyDescriptionProviderService implements IStudyDescriptionProviderService {
+    @Override
+    public String getStudyDescription(int id) {
+        return "http://www.wiz.pwr.wroc.pl/files/prv/id0/Informator/informator%20w-8_29.06.2015.pdf";
+    }
+}
---
+++ stage3/application/src/main/java/external/MockUserAuthorisationService.java
@@ -0,0 +1,13 @@
+package external;
+
+/**
+ * Created by Maciej Wolański
+ * maciekwski@gmail.com
+ * on 05.02.2017.
+ */
+public class MockUserAuthorisationService implements IUserAuthorisationService {
+    @Override
+    public UserData authorizeUser(String login, String passwordHash) {
+        return new UserData();
+    }
+}
