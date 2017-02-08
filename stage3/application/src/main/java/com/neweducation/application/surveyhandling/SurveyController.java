4c3b67295d9a6a2bc34aaecf844c51b603c7fd65
Maciej Wolański <maciekwski@gmail.com>
2017-02-08 15:17:09 +0100

    Added mocked studyDescription service

--- stage3/application/src/main/java/com/neweducation/application/surveyhandling/SurveyController.java
+++ stage3/application/src/main/java/com/neweducation/application/surveyhandling/SurveyController.java
@@ -1,12 +1,41 @@
+
 package com.neweducation.application.surveyhandling;
 
-public class SurveyController {
+        import com.neweducation.application.controllers.AbstractController;
+        import com.neweducation.application.external.IStudyDescriptionProviderService;
+        import com.neweducation.application.external.MockStudyDescriptionProviderService;
+        import com.neweducation.application.security.NotAuthenticatedException;
+        import dtos.UserTo;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RequestParam;
 
-	// zaciagniecie wszystkich sondazy, input - userid
+/**
+ * Created by Maciej Wolański
+ * maciekwski@gmail.com
+ * on 08.02.2017.
+ */
+public class SurveyController extends AbstractController {
 
-	// wybor specjalnosci - input: userid, sondazid, wybor(string, nazwa
-	// specjalnosci)
 
-	// opis specjalnosci - ZAMOCKOWANE - externale, input - idspecjalnosci
+    // zaciagniecie wszystkich sondazy, input - userid
 
+    // wybor specjalnosci - input: userid, sondazid, wybor(string, nazwa
+    // specjalnosci)
+
+    // opis specjalnosci - ZAMOCKOWANE - externale, input - idspecjalnosci
+    IStudyDescriptionProviderService descriptionProviderService = new MockStudyDescriptionProviderService();
+    @RequestMapping(value = "/surveys/{studyId}/getStudyDescription", method = RequestMethod.POST)
+    public ResponseEntity<String> acceptDesignation(@RequestParam(value = "studyId") int studyId, String authToken) {
+        UserTo user = null;
+        try {
+            user = authenticator.authenticateUser("123");
+        } catch (NotAuthenticatedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok().body(this.descriptionProviderService.getStudyDescription(studyId));
+    }
 }
